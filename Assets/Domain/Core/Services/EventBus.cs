8dc08a83b2cfadb351c2ecd02fef7e030e79f3d0
pixel <180dexuan@sina.com>
2025-04-25 01:01:07 +0800

    [feat] 补全事件订阅派发相关

--- Assets/Domain/Core/Services/EventBus.cs
+++ Assets/Domain/Core/Services/EventBus.cs
@@ -8,11 +8,14 @@ namespace Domain.Core.Services
     public class EventBus : Singleton<EventBus>
     {
         private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
+        private readonly Dictionary<Type, List<Action<IDomainEvent>>> _handlers = new Dictionary<Type, List<Action<IDomainEvent>>>();
+        
         public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
         public void AddDomainEvent(IDomainEvent domainEvent)
         {
             _domainEvents.Add(domainEvent);
+            PublishEvent(domainEvent);
         }
 
         public void ClearDomainEvents()
@@ -23,6 +26,38 @@ namespace Domain.Core.Services
         public void RemoveDomainEvent(IDomainEvent domainEvent)
         {
             _domainEvents.Remove(domainEvent);
+        }
+
+        public void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : IDomainEvent
+        {
+            var eventType = typeof(TEvent);
+            if (!_handlers.ContainsKey(eventType))
+            {
+                _handlers[eventType] = new List<Action<IDomainEvent>>();
+            }
+
+            _handlers[eventType].Add((e) => handler((TEvent)e));
+        }
+
+        public void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : IDomainEvent
+        {
+            var eventType = typeof(TEvent);
+            if (_handlers.ContainsKey(eventType))
+            {
+                _handlers[eventType].RemoveAll(h => h.Target == handler.Target && h.Method == handler.Method);
+            }
+        }
+
+        private void PublishEvent(IDomainEvent domainEvent)
+        {
+            var eventType = domainEvent.GetType();
+            if (_handlers.ContainsKey(eventType))
+            {
+                foreach (var handler in _handlers[eventType])
+                {
+                    handler(domainEvent);
+                }
+            }
         }
     }
     
