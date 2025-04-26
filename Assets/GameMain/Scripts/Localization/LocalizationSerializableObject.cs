0687ede052b65d9ee6cd9ebf735e6559360a0a2b
pixel <180dexuan@sina.com>
2025-04-27 00:21:00 +0800

    [feat] 枚举、常量、部分UI组件

---
+++ Assets/GameMain/Scripts/Localization/LocalizationSerializableObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace Pixel
+{
+    public class LocalizationSerializableObject
+    {
+        public string language;
+        public Dictionary<string, string> dic;
+    }
+}
---
+++ Assets/GameMain/Scripts/UI/MenuForm.cs
@@ -0,0 +1,52 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using Pixel;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace Pixel
+{
+    public class MenuForm : UGuiForm
+    {
+        [SerializeField]
+        private GameObject m_QuitButton = null;
+
+        private ProcedureMenu m_ProcedureMenu = null;
+
+       //todo 菜单页面点击事件回调定义
+
+#if UNITY_2017_3_OR_NEWER
+        protected override void OnOpen(object userData)
+#else
+        protected internal override void OnOpen(object userData)
+#endif
+        {
+            base.OnOpen(userData);
+
+            m_ProcedureMenu = (ProcedureMenu)userData;
+            if (m_ProcedureMenu == null)
+            {
+                Log.Warning("ProcedureMenu is invalid when open MenuForm.");
+                return;
+            }
+
+            m_QuitButton.SetActive(Application.platform != RuntimePlatform.IPhonePlayer);
+        }
+
+#if UNITY_2017_3_OR_NEWER
+        protected override void OnClose(bool isShutdown, object userData)
+#else
+        protected internal override void OnClose(bool isShutdown, object userData)
+#endif
+        {
+            m_ProcedureMenu = null;
+
+            base.OnClose(isShutdown, userData);
+        }
+    }
+}
